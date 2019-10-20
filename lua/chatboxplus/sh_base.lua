e4a120994f659ac8942ebcc1bde82312e2d452f2
Samuel Williams <williamssamuel1999@gmail.com>
2019-10-21 00:37:49 +0100

    Added chatbox.lua

--- lua/chatboxplus/sh_base.lua
+++ lua/chatboxplus/sh_base.lua
@@ -1,8 +1,16 @@
+cbp = {}
+
+
 if SERVER then
 	
 end
 
 if CLIENT then
 	include( "chatboxplus/client/vgui/drichertext.lua" )
-	
+	include( "chatboxplus/client/vgui/chatbox.lua" )
+
+	hook.Add( "InitPostEntity", "cfc_cbp_init", function()
+		cbp.buildBox()
+	end )
+
 end
